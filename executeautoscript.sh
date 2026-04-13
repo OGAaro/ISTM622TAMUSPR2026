9e0d4fc3875dce0cfc0de372fdb42b26e4202feb
Aaron Daniels <63876796+OGAaro@users.noreply.github.com>
2026-04-12 21:12:58 -0500

    Update download link for setup script

--- executeautoscript.sh
+++ executeautoscript.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-curl -o /tmp/setup.sh https://raw.githubusercontent.com/OGAaro/ISTM622TAMUSPR2026/main/AUTOSCRIPT1.0__ETL.sh
+curl -o /tmp/setup.sh https://raw.githubusercontent.com/OGAaro/ISTM622TAMUSPR2026/main/AutoScript__V1.sh
 chmod +x /tmp/setup.sh
 bash /tmp/setup.sh
